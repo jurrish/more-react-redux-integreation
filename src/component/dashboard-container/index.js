f0768c39c9fd2a08d013c99761ff045138e43d0c
Frank Iriarte <jurrish@gmail.com>
2017-08-07 14:16:46 -0700

    notes

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 //binds our store to our components from react-redux - this allows us to grab parts of state and pass it to our components through dispatch.
 import { connect } from 'react-redux';
 import {
-  categoryCreate,
-  categoryUpdate,
-  categoryDelete,
+  categoryCreate as categoryActionCreate,
+  categoryUpdate as categoryActionUpdate,
+  categoryDelete as categoryActionDelete,
 } from '../../action/category-actions.js';
+
+//output from our action creator named categoryCreate:
+  //  {
+//   type: 'CATEGORY_CREATE',
+//   payload: ({
+//     title: 'cool',
+//     id: uuid()
+//     timestamp: new Date(),
+//   })
+// }
+
+
 
 import CategoryForm from '../category-form/index.js';
 
@@ -51,11 +63,32 @@ const mapStateToProps = (state) => {
 //mapDispatchToProps gives us access to dispatch and then we can create methods to be this.props.methodName (categoryCreate, categoryUpdate, categoryDelete). these are seen in our this.props.categoryCreate in our componentDidMount
 const mapDispatchToProps = (dispatch, getState) => {
   return {
-    categoryCreate: (category) => dispatch(categoryCreate(category)),
-    categoryUpdate: (category) => dispatch(categoryUpdate(category)),
-    categoryDelete: (category) => dispatch(categoryDelete(category)),
+    categoryCreate: (category) => dispatch(categoryActionCreate(category)),
+    categoryUpdate: (category) => dispatch(categoryActionUpdate(category)),
+    categoryDelete: (category) => dispatch(categoryActionDelete(category)),
   }
 }
 
 //we use connect method to allow DashboardContainer to consume mapStateToProps and mapDispatchToProps
-export default connect( mapStateToProps, mapDispatchToProps )(DashboardContainer);
+//connect takes in a Component and binds default props to it
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardContainer);
+
+// NOTE: can be written like this:
+// --------------------------
+// let bindToStore = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )
+//
+// DashboardContainer = bindToStore(DashboardContainer)
+//
+// export default DashboardContainer;
+//------------------------------
+
+
+//this is kinda like redux
+// let bindPropsToComponent = (props, Component) => return new Component(props);
+// let ExampleDashboard = bindPropsToComponent( { someDefaultProperty: 'SomeDefaultValue' }, DashboardContainer);
